b1411271655e6221bb8010e7014183296f28fe92
fangzhou lin <adamlangwang@gmail.com>
2019-03-14 21:57:04 -0400

    add new utility function

--- utility.py
+++ utility.py
@@ -1,3 +1,6 @@
+import pickle as pl
+
+
 def extract_feature_from_tree(tree, feature_name):
     feature = tree.tree_.feature
     feature_set = set()
@@ -7,3 +10,15 @@ def extract_feature_from_tree(tree, feature_name):
         if feature_name[i] not in feature_set:
             feature_set.add(feature_name[i])
     return feature_set
+
+
+def store_model(file_name, model):
+    with open(file_name, 'wb') as f:
+        pl.dump(model, f)
+
+
+def read_model(file_name):
+    with open(file_name, 'rb') as f:
+        model = pl.load(f)
+        return model
+
